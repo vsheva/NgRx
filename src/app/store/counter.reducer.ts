2ef51077096b50b393632b80105587e8eb98b636
vsheva <valera.sheva@gmail.com>
2024-02-19 22:53:08 -0500

    actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,16 +1,22 @@
-import {createReducer} from "@ngrx/store";
+import {createReducer, on} from "@ngrx/store";
+import {increment} from "./counter.actions";
 
-const initialState =0;
+const initialState =[];
 
-// export const counterReducer = createReducer(
-//   initialState
-// );
+export const counterReducer = createReducer(
+  initialState,
+  on(increment,(state)=>state+1)
+);
 
 
 
-export function counterReducer(state=initialState) {
+/*export function counterReducer(state=initialState) {
   return state
-}
+}*/
+
+
+
+
 
 
 
