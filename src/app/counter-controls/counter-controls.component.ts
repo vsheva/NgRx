a4648ce15828f4be0271da986b33963149addee4
vsheva <valera.sheva@gmail.com>
2024-02-20 13:27:21 -0500

    add data to actions

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -11,7 +11,7 @@ export class CounterControlsComponent {
   constructor(private store: Store) {}
 
   increment() {
-   this.store.dispatch(increment());//!!
+   this.store.dispatch(increment({value:2}));//!!
   }
 
   decrement() {
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -5,7 +5,7 @@ const initialState =0;
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment,(state)=>state+1)
+  on(increment,(state, action)=>state+action.value)
 );
 
 
