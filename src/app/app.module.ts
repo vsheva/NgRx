d39130a653837e951d20411dca8c644c7aa26ccc
vsheva <valera.sheva@gmail.com>
2024-02-19 21:56:23 -0500

    add reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 import { StoreModule } from '@ngrx/store';
+import {counterReducer} from "./store/counter.reducer";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { StoreModule } from '@ngrx/store';
     CounterOutputComponent,
     CounterControlsComponent,
   ],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, StoreModule.forRoot({
+   counter: counterReducer,
+    //auth: authReducer
+  })],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/store/counter.reducer.ts
@@ -0,0 +1,7 @@
+import {createReducer} from "@ngrx/store";
+
+const initialState =0;
+
+export const counterReducer = createReducer(
+  initialState
+);
